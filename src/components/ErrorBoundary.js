8f04e75fd40056cc11540d1d09612ab0cf537589
Ovie Eharisi-Ayomah <dvdhariss@gmail.com>
2024-10-16 10:15:33 +0100

    Converted App.js and ErrorBoundary.js to use hooks instead of classes

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,23 +1,19 @@
-import React, {Component} from "react";
+import React, {useEffect, useState} from "react";
 
-class ErrorBoundary extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            hasError: false
-        }
-    }
+const ErrorBoundary = (props) => {
+    const [hasError, setHasError] = useState(false)
+    
 
-    componentDidCatch(error, info) {
-        this.setState({ hasError: true })
-    }
+    useEffect(() => {
+        setHasError( true );
+    }, [])
 
-    render() {
-        if (this.state.hasError) {
-            return <h1>Oooops. That is not good</h1>
-        }
-        return this.props.children
+    
+    if (!hasError) {
+        return <h1>Oooops. That is not good</h1>
     }
+    return props.children;
+
 }
 
 export default ErrorBoundary
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,5 @@
-import React, {Component} from "react";
+import React, {useEffect, useState} from "react";
 import CardList from "../components/CardList";
-import { robots } from '../robots';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from "../components/ErrorBoundary";
@@ -8,45 +7,38 @@ import './App.css'
 
 
 
-class App extends Component{
-    constructor() {
-        super()
-        this.state = {
-            robots: robots,
-            searchfield: ''
-        }
+const App = () => {
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => setRobots(users))
+    }, []);
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
+    };
+
+    const filteredRobots = robots.filter(robot =>{
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    if (robots.length === 0) {
+        return <h1>Loading</h1>
     }
-
-    // componentDidMount() {
-    //     fetch('https://jsonplaceholder.typicode.com/users')
-    //         .then(response => response.json())
-    //         .then(users => this.setState({robots: users}))
-    // }
-
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value});
-    }
-
-    render() {
-        const { robots, searchfield } = this.state;
-        const filteredRobots = this.state.robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-        if (robots.length === 0) {
-            return <h1>Loading</h1>
-        }
-        return (
-            <div className="tc">
-                <h1 className="f2">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
-                    <ErrorBoundary>
+    return (
+        <div className="tc">
+            <h1 className="f2">RoboFriends</h1>
+            <SearchBox searchChange={onSearchChange}/>
+            <Scroll>
+                <ErrorBoundary>
                     <CardList robots={filteredRobots}/>
-                    </ErrorBoundary>
-                </Scroll>
-            </div>
-        )
-    }   
+                </ErrorBoundary>
+            </Scroll>
+        </div>
+    ) 
 }
+
 
 export default App
